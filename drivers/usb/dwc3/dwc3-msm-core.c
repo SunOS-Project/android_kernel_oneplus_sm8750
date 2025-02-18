6adf985d065f3b100ce1b5e21d0f60c394ec32fd
Wesley Cheng <quic_wcheng@quicinc.com>
2025-02-18 12:34:17 -0800

    usb: dwc3: dwc3-msm-core: Resume SS PHY if in 2/4LN DP mode always
    commit 28feebbd6f30 ("usb: dwc3-msm: Allow DP mode only after host mode
    has started")
    
    Added a check for dp_state before allowing the SSPHY to be powered down.
    However, it avoided updating the logic that was present in determining if
    the SSPHY needed to be resumed from the normal resume path.
    (dwc3_msm_resume())
    
    This led to a situation where a 2LN DP adapter w/o a SSUSB hub, would
    keep the DWC3 wrapper's source clock mux to rely on PIPE clock being
    available.  However, when a bus resume scenario occurs, the
    dwc3_msm_resume() would avoid powering up the SSPHY, and the xHCI resume
    path would be running w/o a source clock to the controller.
    
    Change-Id: I7def6aecd003be386e6daad98463975febc9a4ee
    Signed-off-by: Wesley Cheng <quic_wcheng@quicinc.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -4347,8 +4347,8 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 	if (dwc3_msm_get_max_speed(mdwc) >= USB_SPEED_SUPER &&
 			mdwc->lpm_flags & MDWC3_SS_PHY_SUSPEND) {
 		dwc3_set_ssphy_orientation_flag(mdwc);
-		if (!mdwc->in_host_mode || mdwc->disable_host_ssphy_powerdown ||
-			(mdwc->in_host_mode && mdwc->max_rh_port_speed != USB_SPEED_HIGH))
+
+		if (!(mdwc->ss_phy->flags & PHY_SS_PHY_DYNAMIC_POWERDOWN))
 			usb_phy_set_suspend(mdwc->ss_phy, 0);
 
 		mdwc->ss_phy->flags &= ~DEVICE_IN_SS_MODE;
@@ -6544,6 +6544,12 @@ static int dwc3_msm_host_ss_powerdown(struct dwc3_msm *mdwc)
 {
 	u32 reg;
 
+	/*
+	 * Conditions for allowing dynamic powerdown of the SS PHY:
+	 *   1. The feature is not disabled by the DT property
+	 *   2. Not currently in a DP active state
+	 *   3. Connected device's speed is not super-speed
+	 */
 	if (mdwc->disable_host_ssphy_powerdown || mdwc->dp_state ||
 		dwc3_msm_get_max_speed(mdwc) < USB_SPEED_SUPER)
 		return 0;
@@ -6556,6 +6562,7 @@ static int dwc3_msm_host_ss_powerdown(struct dwc3_msm *mdwc)
 	usb_phy_notify_disconnect(mdwc->ss_phy,
 					USB_SPEED_SUPER);
 	usb_phy_set_suspend(mdwc->ss_phy, 1);
+	mdwc->ss_phy->flags |= PHY_SS_PHY_DYNAMIC_POWERDOWN;
 
 	return 0;
 }
@@ -6578,6 +6585,7 @@ static int dwc3_msm_host_ss_powerup(struct dwc3_msm *mdwc)
 	reg = dwc3_msm_read_reg(mdwc->base, EXTRA_INP_REG);
 	reg &= ~EXTRA_INP_SS_DISABLE;
 	dwc3_msm_write_reg(mdwc->base, EXTRA_INP_REG, reg);
+	mdwc->ss_phy->flags &= ~PHY_SS_PHY_DYNAMIC_POWERDOWN;
 
 	return 0;
 }
@@ -6964,6 +6972,14 @@ static int dwc3_otg_start_host(struct dwc3_msm *mdwc, int on)
 			dwc3_msm_host_ss_powerup(mdwc);
 			dwc3_msm_clear_dp_only_params(mdwc);
 		}
+
+		/*
+		 * Need to explicitly clear here, as changes were made to avoid
+		 * running dwc3_msm_host_ss_powerup on host mode teardown, due to
+		 * subsequent USB enumeration issues when moving from 4LN DP to
+		 * device mode.
+		 */
+		mdwc->ss_phy->flags &= ~PHY_SS_PHY_DYNAMIC_POWERDOWN;
 
 		/*
 		 * Performing phy disconnect before flush work to
