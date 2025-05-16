torvalds/linux

net: stmmac: Add support for PHY interrupts
Add support for PHY interrupts and read pin control.

Change-Id: I38b20abcd1bb967bde396b539f5f5de5f5da1c9b
Signed-off-by: Sarosh Hasan <quic_sarohasa@quicinc.com>

drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-gpio.c
@@ -6,6 +6,7 @@
 #include <linux/platform_device.h>
 #include <linux/phy.h>
 #include <linux/regulator/consumer.h>
+#include <linux/of_gpio.h>
 
 #include "stmmac.h"
 #include "dwmac-qcom-ethqos.h"
@@ -14,6 +15,44 @@
 #define EMAC_VREG_RGMII_NAME "vreg_rgmii"
 #define EMAC_VREG_EMAC_PHY_NAME "vreg_emac_phy"
 #define EMAC_VREG_RGMII_IO_PADS_NAME "vreg_rgmii_io_pads"
+
+static int setup_gpio_input_common
+	(struct device *dev, const char *name, int *gpio)
+{
+	int ret = 0;
+
+	if (of_find_property(dev->of_node, name, NULL)) {
+		*gpio = ret = of_get_named_gpio(dev->of_node, name, 0);
+		if (ret >= 0) {
+			ret = gpio_request(*gpio, name);
+			if (ret) {
+				ETHQOSERR("Can't get GPIO %s, ret = %d\n",
+					  name, *gpio);
+				*gpio = -1;
+				return ret;
+			}
+
+			ret = gpio_direction_input(*gpio);
+			if (ret) {
+				ETHQOSERR("failed GPIO %s direction ret=%d\n",
+					  name, ret);
+				return ret;
+			}
+		} else {
+			if (ret == -EPROBE_DEFER)
+				ETHQOSERR("get EMAC_GPIO probe defer\n");
+			else
+				ETHQOSERR("can't get gpio %s ret %d\n", name,
+					  ret);
+			return ret;
+		}
+	} else {
+		ETHQOSERR("can't find gpio %s\n", name);
+		ret = -EINVAL;
+	}
+
+	return ret;
+}
 
 int ethqos_init_reqgulators(struct qcom_ethqos *ethqos)
 {
@@ -125,4 +164,92 @@ void ethqos_disable_regulators(struct qcom_ethqos *ethqos)
 		regulator_disable(ethqos->gdsc_emac);
 		ethqos->gdsc_emac = NULL;
 	}
+}
+
+void ethqos_free_gpios(struct qcom_ethqos *ethqos)
+{
+	if (gpio_is_valid(ethqos->gpio_phy_intr_redirect))
+		gpio_free(ethqos->gpio_phy_intr_redirect);
+	ethqos->gpio_phy_intr_redirect = -1;
+}
+
+int ethqos_init_pinctrl(struct device *dev)
+{
+	struct pinctrl *pinctrl;
+	struct pinctrl_state *pinctrl_state;
+	int i = 0;
+	int num_names;
+	const char *name;
+	int ret = 0;
+
+	pinctrl = devm_pinctrl_get(dev);
+	if (IS_ERR_OR_NULL(pinctrl)) {
+		ret = PTR_ERR(pinctrl);
+		ETHQOSERR("Failed to get pinctrl, err = %d\n", ret);
+		return ret;
+	}
+
+	num_names = of_property_count_strings(dev->of_node, "pinctrl-names");
+	if (num_names < 0) {
+		dev_err(dev, "Cannot parse pinctrl-names: %d\n",
+			num_names);
+		return num_names;
+	}
+
+	for (i = 0; i < num_names; i++) {
+		ret = of_property_read_string_index(dev->of_node,
+						    "pinctrl-names",
+						    i, &name);
+
+		if (!strcmp(name, PINCTRL_STATE_DEFAULT))
+			continue;
+
+		pinctrl_state = pinctrl_lookup_state(pinctrl, name);
+		if (IS_ERR_OR_NULL(pinctrl_state)) {
+			ret = PTR_ERR(pinctrl_state);
+			ETHQOSERR("lookup_state %s failed %d\n", name, ret);
+			return ret;
+		}
+
+		ETHQOSINFO("pinctrl_lookup_state %s succeeded\n", name);
+
+		ret = pinctrl_select_state(pinctrl, pinctrl_state);
+		if (ret) {
+			ETHQOSERR("select_state %s failed %d\n", name, ret);
+			return ret;
+		}
+
+		ETHQOSINFO("pinctrl_select_state %s succeeded\n", name);
+	}
+
+	return ret;
+}
+
+int ethqos_init_gpio(struct qcom_ethqos *ethqos)
+{
+	int ret = 0;
+
+	ethqos->gpio_phy_intr_redirect = -1;
+
+	ret = ethqos_init_pinctrl(&ethqos->pdev->dev);
+	if (ret) {
+		ETHQOSERR("ethqos_init_pinctrl failed");
+		return ret;
+	}
+
+	ret = setup_gpio_input_common(&ethqos->pdev->dev,
+				      "qcom,phy-intr-redirect",
+			&ethqos->gpio_phy_intr_redirect);
+
+	if (ret) {
+		ETHQOSERR("Failed to setup <%s> gpio\n",
+			  "qcom,phy-intr-redirect");
+		goto gpio_error;
+	}
+
+	return ret;
+
+gpio_error:
+	ethqos_free_gpios(ethqos);
+	return ret;
 }

drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1165,7 +1165,19 @@ static int stmmac_init_phy(struct net_device *dev)
 			netdev_err(priv->dev, "no phy at addr %d\n", addr);
 			return -ENODEV;
 		}
+		if (phy_intr_en) {
+			phydev->irq = PHY_MAC_INTERRUPT;
+			phydev->interrupts =  PHY_INTERRUPT_ENABLED;
 
+			if (phydev->drv && phydev->drv->config_intr &&
+			    !phydev->drv->config_intr(phydev)) {
+				pr_debug(" qcom-ethqos: %s config_phy_intr successful\n",
+					 __func__);
+		}
+	} else {
+		pr_err("stmmac phy polling mode\n");
+		phydev->irq = PHY_POLL;
+	}
 		ret = phylink_connect_phy(priv->phylink, phydev);
 	} else {
 		fwnode_handle_put(phy_fwnode);

SunOS-Project/android_kernel_oneplus_sm8750

Signed-off-by: Navjyot SINGH <quic_navjsing@quicinc.com>

drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-gpio.c
@@ -4,8 +4,9 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
-#include <linux/platform_device.h>
+#include <linux/of_gpio.h>
 #include <linux/phy.h>
+#include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
 
 #include "dwmac-qcom-ethqos.h"
@@ -15,6 +16,44 @@
 #define EMAC_VREG_RGMII_NAME "vreg_rgmii"
 #define EMAC_VREG_EMAC_PHY_NAME "vreg_emac_phy"
 #define EMAC_VREG_RGMII_IO_PADS_NAME "vreg_rgmii_io_pads"
+
+static int setup_gpio_input_common(struct device *dev,
+				   const char *name, int *gpio)
+{
+	int ret = 0;
+
+	if (of_find_property(dev->of_node, name, NULL)) {
+		*gpio = ret = of_get_named_gpio(dev->of_node, name, 0);
+		if (ret >= 0) {
+			ret = gpio_request(*gpio, name);
+			if (ret) {
+				ETHQOSERR("Can't get GPIO %s, ret = %d\n",
+					  name, *gpio);
+				*gpio = -1;
+				return ret;
+			}
+
+			ret = gpio_direction_input(*gpio);
+			if (ret) {
+				ETHQOSERR("failed GPIO %s direction ret=%d\n",
+					  name, ret);
+				return ret;
+			}
+		} else {
+			if (ret == -EPROBE_DEFER)
+				ETHQOSDBG("get EMAC_GPIO probe defer\n");
+			else
+				ETHQOSERR("can't get gpio %s ret %d\n", name,
+					  ret);
+			return ret;
+		}
+	} else {
+		ETHQOSERR("can't find gpio %s\n", name);
+		ret = -EINVAL;
+	}
+
+	return ret;
+}
 
 int ethqos_init_regulators(struct qcom_ethqos *ethqos)
 {
@@ -126,4 +165,92 @@ void ethqos_disable_regulators(struct qcom_ethqos *ethqos)
 		regulator_disable(ethqos->gdsc_emac);
 		ethqos->gdsc_emac = NULL;
 	}
+}
+
+void ethqos_free_gpios(struct qcom_ethqos *ethqos)
+{
+	if (gpio_is_valid(ethqos->gpio_phy_intr_redirect))
+		gpio_free(ethqos->gpio_phy_intr_redirect);
+	ethqos->gpio_phy_intr_redirect = -1;
+}
+
+int ethqos_init_pinctrl(struct device *dev)
+{
+	struct pinctrl *pinctrl;
+	struct pinctrl_state *pinctrl_state;
+	int i = 0;
+	int num_names;
+	const char *name;
+	int ret = 0;
+
+	pinctrl = devm_pinctrl_get(dev);
+	if (IS_ERR_OR_NULL(pinctrl)) {
+		ret = PTR_ERR(pinctrl);
+		ETHQOSERR("Failed to get pinctrl, err = %d\n", ret);
+		return ret;
+	}
+
+	num_names = of_property_count_strings(dev->of_node, "pinctrl-names");
+	if (num_names < 0) {
+		dev_err(dev, "Cannot parse pinctrl-names: %d\n",
+			num_names);
+		return num_names;
+	}
+
+	for (i = 0; i < num_names; i++) {
+		ret = of_property_read_string_index(dev->of_node,
+						    "pinctrl-names",
+						    i, &name);
+
+		if (!strcmp(name, PINCTRL_STATE_DEFAULT))
+			continue;
+
+		pinctrl_state = pinctrl_lookup_state(pinctrl, name);
+		if (IS_ERR_OR_NULL(pinctrl_state)) {
+			ret = PTR_ERR(pinctrl_state);
+			ETHQOSERR("lookup_state %s failed %d\n", name, ret);
+			return ret;
+		}
+
+		ETHQOSDBG("pinctrl_lookup_state %s succeeded\n", name);
+
+		ret = pinctrl_select_state(pinctrl, pinctrl_state);
+		if (ret) {
+			ETHQOSERR("select_state %s failed %d\n", name, ret);
+			return ret;
+		}
+
+		ETHQOSDBG("pinctrl_select_state %s succeeded\n", name);
+	}
+
+	return ret;
+}
+
+int ethqos_init_gpio(struct qcom_ethqos *ethqos)
+{
+	int ret = 0;
+
+	ethqos->gpio_phy_intr_redirect = -1;
+
+	ret = ethqos_init_pinctrl(&ethqos->pdev->dev);
+	if (ret) {
+		ETHQOSERR("ethqos_init_pinctrl failed\n");
+		return ret;
+	}
+
+	ret = setup_gpio_input_common(&ethqos->pdev->dev,
+				      "qcom,phy-intr-redirect",
+			&ethqos->gpio_phy_intr_redirect);
+
+	if (ret) {
+		ETHQOSERR("Failed to setup <%s> gpio\n",
+			  "qcom,phy-intr-redirect");
+		goto gpio_error;
+	}
+
+	return ret;
+
+gpio_error:
+	ethqos_free_gpios(ethqos);
+	return ret;
 }

drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1180,7 +1180,19 @@ static int stmmac_init_phy(struct net_device *dev)
 			netdev_err(priv->dev, "no phy at addr %d\n", addr);
 			return -ENODEV;
 		}
+		if (phy_intr_en) {
+			phydev->irq = PHY_MAC_INTERRUPT;
+			phydev->interrupts =  PHY_INTERRUPT_ENABLED;
 
+			if (phydev->drv && phydev->drv->config_intr &&
+			    !phydev->drv->config_intr(phydev)) {
+				pr_debug(" qcom-ethqos: %s config_phy_intr successful\n",
+					 __func__);
+			}
+		} else {
+			pr_debug("stmmac phy polling mode\n");
+			phydev->irq = PHY_POLL;
+		}
 		ret = phylink_connect_phy(priv->phylink, phydev);
 	} else {
 		fwnode_handle_put(phy_fwnode);